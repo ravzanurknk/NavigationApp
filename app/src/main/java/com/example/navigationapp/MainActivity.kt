184eea34562fbc2f65f26efcae2144777e3bee7c
Ravzanur Konuk <ravzanurkonukk@gmail.com>
2021-12-02 14:17:54 +0300

    Deleted unused files. Code refactore.

--- app/src/main/java/com/example/navigationapp/MainActivity.kt
+++ app/src/main/java/com/example/navigationapp/MainActivity.kt
@@ -2,10 +2,48 @@ package com.example.navigationapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.fragment.app.FragmentContainerView
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var bottomNavView: BottomNavigationView
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        bottomNavView = findViewById(R.id.nav_view)
+        navController = findNavController(R.id.nav_host_fragment)
+        bottomNavView.setupWithNavController(navController)
+
+
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navigation_home,
+                R.id.navigation_search
+            )
+        )
+
+        setupActionBarWithNavController(navController, appBarConfiguration)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            android.R.id.home -> {
+                navController.navigateUp()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    override fun onBackPressed() {
+        super.onBackPressed()
     }
 }
